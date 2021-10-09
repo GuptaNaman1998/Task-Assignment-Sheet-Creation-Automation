b5f784241dfa209cf4d62f78162c7adeea8ef221
Naman Gupta <54541309+GuptaNaman1998@users.noreply.github.com>
2021-10-10 01:47:33 +0530

    Eliminated user inputs + the end note

--- TaskAssignmentSheetGenerator.py
+++ TaskAssignmentSheetGenerator.py
@@ -15,38 +15,32 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
     
 def AddSheet(ws,week,POC):
-    # Filling the Header with the values
     ws.append(['Shift','Engineer Name','Case','Type','Case','Type','Case','Type','Case','Type'])
     x=week.keys()
     code = 0
     for i in x:
-        # Filling the cells with the values from the Dictionary and checking for POCs
-        if i in POC:
+        if i in POC or "POC" in week[i]:
             code+=1
-            ws.append(["POC : "+week[i],i])
+            ws.append([week[i],i])
+            ws.append(["",""])
         else:
             ws.append([week[i],i])
 
-    # Format the cell colour to be as desired
     OrangeFill = PatternFill(start_color='FFC000',end_color='FFC000',fill_type='solid')
     BlueFill = PatternFill(start_color='00B0F0',end_color='00B0F0',fill_type='solid')
     BlackFill = PatternFill(start_color='000000',end_color='000000',fill_type='solid')
+    WhiteFill = PatternFill(start_color='FF0000',end_color='FF0000',fill_type='solid')
     GreenFill = PatternFill(start_color='33CC33',end_color='33CC33',fill_type='solid')
-
-    # Format the cell border
+	
     ThinBorder = Border(left=Side(style='thick'),right=Side(style='thick'),top=Side(style='thick'),bottom=Side(style='thick'))
 
-    # Format the Font size type and appearance
     BlackFont= Font(name='Cambria',size=11,bold=True,italic=True)
     WhiteFont = Font(name='Cambria',size=11,bold=True,italic=True,color='FFFFFF')
 
-    # Format the text alignment in the cells
     Al= Alignment(horizontal='center',vertical='center')
 
-    # Applying the Styles & formats to the cells
     for col in range(1, ws.max_column + 1):
         cell_header = ws.cell(1, col)
-        # print(cell_header.value)
         cell_header.fill = GreenFill
         cell_header.border = ThinBorder
         cell_header.font = BlackFont
@@ -98,7 +92,6 @@ def AddSheet(ws,week,POC):
         cell[0].font = BlackFont
         cell[0].alignment = Al
         
-    # Marking the POC with different cell colour and font colour
     for row in range(1, ws.max_row + 1):
         cell_header = ws.cell(row, 1)
         if "POC" in cell_header.value:
@@ -107,8 +100,11 @@ def AddSheet(ws,week,POC):
             cell_header = ws.cell(row, 2)
             cell_header.fill = BlackFill
             cell_header.font = WhiteFont 
+        if "" == cell_header.value:
+            for cell in ws['A{}:J{}'.format(row,row)]:
+                for ele in cell:
+                    ele.fill = WhiteFill
 
-    # Setting the Column width
     ws.column_dimensions['A'].width = 10
     ws.column_dimensions['B'].width = 26           
     ws.column_dimensions['C'].width = 12            
@@ -122,34 +118,8 @@ def AddSheet(ws,week,POC):
     return code
 
 def ExtractData(file):
-    '''
-        This code reads the Excel sheet and cleans or sorts out the data we require.
-        After the sorting and filtering of the data it returns a list of Required Data data.
-        
-        Data We Have:
-            Name of the Engineers
-            Days of the month
-            Days of the week
-            Shift Letter specifying the shift of that engineer
-            POC Details
-            Days in each shift per engineer
-            PH & PL
-            OnCall Details
-            
-        Data We Require To be returned:
-            Name of the Engineers
-            Days of the month
-            Days of the week
-            Shift Letter specifying the shift of that engineer
-            PH & PL
-    '''
-    # Asking for the file name to be read
-    try:
-        wb = load_workbook(file+'.xlsx', data_only = True)
-        logging.info("Successfully opened the excel file")
-    except:
-        logging.error("Unable to open the excel file!!.. Please check if the file is opened by another program already. If so, please close and retry!")
-        print("Please check if the file is opened by another program already")
+    wb = load_workbook(file, data_only = True)
+    logging.info("Successfully opened the excel file")
     ws = wb.active
     sh = wb[ws.title]
     temp=[]
@@ -163,16 +133,6 @@ def ExtractData(file):
         temp.append(tempr)
         
 def roster(flag,temp):
-    '''
-        This code takes a list of Required Data as input and returns a dictionary.
-        
-        Arguments/Parameters:
-            Flag - It hols the value of the day of the month on the First monday
-            temp - It is the list of the required filtered data
-            
-        Returned Variable:
-            A dictionary sorted based on the values of the dictionary, with Key as the engineer name & Value as their shift letter
-    '''
     week=[]
     if flag <= 27:
         x=5
@@ -182,38 +142,23 @@ def roster(flag,temp):
         week1={}
         for i in temp[2:]:
             shifts = i[flag+ele]
-            if shifts == 'L' :
-                shifts='Leave'
-            if shifts == None:
-                shifts='NA'
+            if shifts in ['L', None ,'HK','TR']:
+                continue
+            if shifts == "B":
+                shifts='G'
             week1[i[0]] = shifts 
         week.append({k: v for k, v in sorted(week1.items(), key=lambda item: item[1])})
     return week
 
 def createExcel(week,flag,fname):
-    '''
-        This code creates the Excel sheet as an output.
-        
-        Arguments/Parameters:
-            Flag - It hols the value of the day of the month on the First monday
-            Week - It is the Dictionary of the required filtered data
-            
-        Returned Variable:
-            A dictionary sorted based on the values of the dictionary, with Key as the engineer name & Value as their shift letter
-    '''
     wb = Workbook()
+    POC=[]
     
-    # Asking for POC details from the user
-    Val=input("Please enter the names of POCs followed by a ',' :")
-    POC=Val.split(",")
-    
-    # Logic for days of the last week 
     if flag <= 27:
         x=5
     else:
         x=32-flag
         
-    # Looping the function call over the number of days
     code = []
     for i in range(x):
         ws = wb.create_sheet(str(flag+i), i)
@@ -222,31 +167,36 @@ def createExcel(week,flag,fname):
     weekNumber = datetime.date(d.tm_year,d.tm_mon,d.tm_mday).strftime("%V")
     if min(code)<4:
         logging.warning(str(min(code))+" POCs were found!... please check if their names were mis-spelled :"+" ".join(POC)+" week"+weekNumber)
-    # Saving the output in the predefined folder
     logging.info("Successfully Identified the Date on Monday for week"+weekNumber+" Date: "+str(flag)+" "+c[0][:3]+" "+c[1])
-    wb.save('Output/Week-'+weekNumber+'.xlsx')
+    try:
+        wb.save('Output/Week-'+weekNumber+'.xlsx')
+        logging.info("Successfully saved the excel file")
+    except PermissionError:
+        logging.error("Unable to open the excel file!!.. Please check if the file is opened by another program already. If so, please close and retry!")
+        print("Please check if the file is opened by another program already")
+        print("Due to file being already open elsewhere process terminated!!!... Please close the file and retry.")
+        sys.exit()
     
 if __name__ == "__main__":
     fmtstr = "%(asctime)s | %(levelname)s | Line:%(lineno)s | %(funcName)s : %(message)s"
     datestr = "%d-%m-%Y %I:%M:%S %p"
-    # lognm = 'Logs\\'    
-    # logging.basicConfig(filename = 'Logs\output.log',level = logging.DEBUG,filemode = "w",format = fmtstr,datefmt = datestr)    
-    # maxBytes=1000 means 1kb
+	
     logging.basicConfig(handlers=[RotatingFileHandler('Logs\output.log', maxBytes=1000000, backupCount=10)],level = logging.DEBUG,format = fmtstr,datefmt = datestr)
     warnings.simplefilter("ignore")
-    print("*Note: entered file name should be in the month-year format eg. August-2021\n")
-    file = input("Enter the file name to read (without extension):")
+    c = [ele for ele in os.listdir() if ".xlsx" in ele]
+    file = c[0]
     tries = 1
-    while not os.path.isfile(file+'.xlsx') and tries <5:          
-        logging.warning("Unable to locate the entered file in the current directory!!... The file "+file+'.xlsx '+"NOT Found")        
+    while not os.path.isfile(file) and tries <5:          
+        logging.warning("Unable to locate the entered file in the current directory!!... The file "+file+"NOT Found")        
         cls()
         print("*Error: entered file name should be in the month-year format eg. August-2021 \nand should exist in the same path as the .exe file!...\n")
-        file = input("Please Enter the file name to read again (without extension):")
+        c = [ele for ele in os.listdir() if ".xlsx" in ele]
+        file = c[0]
         tries += 1
         if tries == 5:
-            logging.error("Unable to locate the entered file in the current directory!!... The file "+file+'.xlsx '+"NOT Found")
+            logging.error("Unable to locate the entered file in the current directory!!... The file "+file+"NOT Found")
             cls()
-            print("You've exhausted your 5 retry counts!!... was unable to find a file named : "+file+".xlsx")
+            print("You've exhausted your 5 retry counts!!... was unable to find a file named : "+file)
             exi=input("\n Press any key to exit...  ")
             sys.exit()
     val = ExtractData(file)
@@ -254,18 +204,16 @@ if __name__ == "__main__":
     temp=[]
     for i in val:
         temp.append(i[1:33])
-        
     for i in range(len(temp[0])):
         if temp[0][i]=='Mon':
             flag=i
             break
     while flag<32:
-        fname = str(flag)+" "+c[0][:3]+" "+c[1]
+        fname = str(flag)+" "+c[0][:3]+" "+c[1][:-5]
         week1=roster(flag,temp)
         createExcel(week1,flag,fname)
         flag+=7
      
-    cls()
     print("""    
     
 Please collect your files from the output folder.
@@ -276,27 +224,3 @@ Do follow me on GitHub at: https://github.com/GuptaNaman1998
            
     """)
     exi=input("Press any key to exit...  ")
-    
-        
-"""
-Exceptions/Lowlights:
-        
-        1. February month the flag threshold will need to be changed
-        2. For all the months only whole weeks are being considered, Basically the code finds the first cell where monday appears and considers it as day 1.
-        Which will cause a week of missing roster.
-        3. Have to add the leaves, Public Holidays and training part manually
-        4. For every month's roster the code will ignore the days before the first monday but will consider the last week even if it is just a monday.
-        This will get an errored roster as if a person was on leave on monday the code will add leave for the whole week.
-        
-Next Implementation Steps:
-
-        1. Convert the file name preset value to user input.
-        2. Convert this script to either a GUI application or an executable ".exe" file.
-        3. Need to add naming convention as we need the file to be named week whatever the count is.
-        
-Subjeactive Addition:
-
-        1. Currently the naming convention of case assignment file is Week+ " Count of the week " which leads to sorting issues.
-        What if we create a folder heirarcy wherein you don't have to find which file has the highest week written on it, Just have to scroll to the end of the folder.
-        2. POC Addition/Incusion in the case assignment sheet generated as an output.
-"""
